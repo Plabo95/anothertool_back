042df55fc1661636a7fc483e548c83a54409ebaa
pablo.novo <pablo.novo@ara-tech.es>
2023-01-16 17:19:09 +0100

    user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **kwargs):
+        "Creates and saves a new user"
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    "CUstom user model without username, only email"
+    email = models.EmailField(max_length=255 ,unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
