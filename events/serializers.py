909f5138d0dc1ec5e938c3f135f0697ef62f0dd8
plabo <plabonovo@gmail.com>
2023-01-19 20:38:31 +0100

    event models

---
+++ events/serializers.py
@@ -0,0 +1,25 @@
+from models import *
+from rest_framework import serializers
+
+
+class EventSerializer(serializers.ModelSerializer):
+    #client = ClientSerializer(many=False)
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    client_name = serializers.ReadOnlyField()
+    service_name = serializers.ReadOnlyField()
+    service_color = serializers.ReadOnlyField()
+
+    def validate_ended(self,value):
+        return value
+    def validate_not_paid(self, value):
+        return value
+
+class NextEventSerializer(serializers.ModelSerializer):
+    service = ServiceSerializer(many=False)
+    client = ClientSerializer(many=False)
+    class Meta:
+        model = Event
+        fields = '__all__'
--- events/views.py
+++ events/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from models import *
+
+from rest_framework.decorators import api_view, permission_classes
+
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def events(request, pk):
+    events = Event.objects.filter(user=pk).order_by('-start')
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def nextEvents(request, pk):
+    today = datetime.now().date()
+    tomorrow = today + timedelta(1)
+    today_start = datetime.combine(today, time())
+    today_end = datetime.combine(tomorrow, time())
+
+    events = Event.objects.filter(user=pk, start__range=(today_start, today_end)).order_by('start')
+    serializer = NextEventSerializer(events, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createEvent(request):
+    serializer = EventSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteEvent(request, pk, sk):
+    event = Event.objects.get(user=pk, id=sk)
+    client_id = event.client.id
+    event.delete()
+    sinpa = Event.objects.filter(user=pk, client=client_id, paid=False).count()      #Busco si tiene mas citas sin pagar
+    if sinpa<1:
+        Client.objects.filter(user=pk, id=client_id).update(moroso=False)
+    return Response()
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def updateEvent(request, pk, sk):
+    event = Event.objects.get(user=pk, id=sk)
+    serializer = EventSerializer(instance= event, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
