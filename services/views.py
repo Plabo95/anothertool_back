9a63006740b15f1a9d22fdfe69be25b279650607
plabo <plabonovo@gmail.com>
2023-01-23 10:52:57 +0100

    service view + serializer

--- services/views.py
+++ services/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+#rest fw
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.views import APIView, Response
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from serializers import ServiceSerializer
+from models import *
 
 # Create your views here.
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def services(request,pk):
+    services = Service.objects.filter(user=pk)
+    serializer = ServiceSerializer(services, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createService(request):
+    serializer = ServiceSerializer(data=request.data)
+    print(serializer.initial_data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def updateService(request, pk, sk):
+    service = Service.objects.get(user=pk, id = sk)
+    serializer = ServiceSerializer(instance= service, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteService(request, pk, sk):
+    service = Service.objects.get(user=pk, id = sk)
+    service.delete()
+    return Response()
