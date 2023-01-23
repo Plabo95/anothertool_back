90959f2677b2513b6a440dde49f4705329886e23
plabo <plabonovo@gmail.com>
2023-01-23 13:36:58 +0100

    urls and routes

--- clients/views.py
+++ clients/views.py
@@ -9,14 +9,14 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def clients(request, pk):
     clients = Client.objects.filter(user=pk)
     serializer = ClientSerializer(clients, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def createClient(request):
     serializer = ClientSerializer(data=request.data)
     if serializer.is_valid():
@@ -25,7 +25,7 @@ def createClient(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def updateClient(request, pk, sk):
     client = Client.objects.get(user=pk, id = sk)
     serializer = ClientSerializer(instance= client, data=request.data)
@@ -35,7 +35,7 @@ def updateClient(request, pk, sk):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
-@permission_classes([IsAuthenticated])
+#@permission_classes([IsAuthenticated])
 def deleteClient(request, pk, sk):
     client = Client.objects.get(user=pk, id = sk)
     client.delete()
