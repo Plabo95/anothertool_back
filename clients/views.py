a514fcd82e0bb5ef72ac65032c60ab25ba62b7bd
plabo <plabonovo@gmail.com>
2023-01-31 18:39:03 +0100

    views and required auth

--- clients/views.py
+++ clients/views.py
@@ -2,14 +2,13 @@ from clients.models import *
 from clients.serializers import *
 
 #RF
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from rest_framework import viewsets
 
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-    #permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def perform_create(self, serializer):
-        print(self.request.user)
         serializer.save(user=self.request.user)
